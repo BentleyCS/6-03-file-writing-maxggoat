8965710b467799ff0eb557e4b390281806ffb8b3
maxggoat <mgorodnichenko28@bentleyschool.org>
2026-03-04 11:37:23 -0800

    Add files via upload

--- test_CSP_6_03_Writing_to_files.py
+++ test_CSP_6_03_Writing_to_files.py
@@ -16,6 +16,18 @@ def test_writeFile():
         assert contents == "1\n2\n3\n"
 
 
+def test_sortNames():
+
+    writeFile(["3\n", "2\n", "1\n"], "source.txt")
+
+
+    sortNames("source.txt", "target.txt")
+
+    with open("target.txt") as file:
+        sorted_names = [line.strip() for line in file]
+
+    assert sorted_names == ["1", "2", "3"]
+
 
 
 
